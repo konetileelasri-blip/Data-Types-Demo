264598b0b0ceb579b8e8f1636e0d515fd6961edd
konetileelasri-blip <konetileelasri@gmail.com>
2026-08-14 10:43:06 +0530

    Create  DataTypeDemo.java

---
+++ DataTypeDemo.java
@@ -0,0 +1,22 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+
+        byte b = 10;
+        short s = 1000;
+        int i = 50000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 25.75;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + bool);
+    }
+}
